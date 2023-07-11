3391ca8ec15c338eb781fa627b88c4832892b85d
Amber Kimbrough (AJ) <94200525+AJKimbrough@users.noreply.github.com>
2023-07-11 17:28:20 -0500

    complete lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,7 @@
 def oxford_comma(items):
-    return None
+    if len(items) < 3:
+        return ' and '.join(items)
+    items[-1] = 'and ' + items[-1]
+    return ', '.join(items)
+
+print(oxford_comma(["fiddleheads", "okra", "kohlrabi"]))
